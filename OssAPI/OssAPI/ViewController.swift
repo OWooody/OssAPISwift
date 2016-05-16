f4cd1e00f5f0cd1a5deec4f5d65279a89b857045
Osama Al-Najjar <najjariclub@gmail.com>
2016-05-16 19:03:45 +0300

    Test GitHub

--- OssAPI/OssAPI/ViewController.swift
+++ OssAPI/OssAPI/ViewController.swift
@@ -21,7 +21,8 @@ class ViewController: UIViewController {
         api.call(URLRequest) { data, error in
             
             //log.info(data)
-            let salon = SalonService.parse(data!)
+            let s: Salon!
+            //let salon = api.parse(data!)//SalonService.parse(data!)
             print(salon[0].branches)
             log.error(error)
         }
